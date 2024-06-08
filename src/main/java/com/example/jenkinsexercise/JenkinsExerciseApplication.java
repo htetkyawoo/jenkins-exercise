40f986f1ff35a78c0499c49b01132616bd4e1d78
Htet Kyaw Oo <htetkyawu@gmail.com>
2024-06-08 15:33:45 +0630

    create hello controller

--- src/main/java/com/example/jenkinsexercise/JenkinsExerciseApplication.java
+++ src/main/java/com/example/jenkinsexercise/JenkinsExerciseApplication.java
@@ -2,6 +2,10 @@ package com.example.jenkinsexercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JenkinsExerciseApplication {
@@ -10,4 +14,14 @@ public class JenkinsExerciseApplication {
 		SpringApplication.run(JenkinsExerciseApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/")
+class Hello {
+
+	@GetMapping
+	public String greet(@RequestParam(defaultValue = "World", required = false) String value) {
+		return "Hello " + value;
+	}
 }
